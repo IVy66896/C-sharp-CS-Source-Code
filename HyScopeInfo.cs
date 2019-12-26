adb900e64d251e4469a1fb0d7004b65370a64abf
peter0817 <59242083+peter0817@users.noreply.github.com>
2019-12-26 04:19:34 +0000

    Create HyScopeInfo.cs

---
+++ HyScopeInfo.cs
@@ -0,0 +1,33 @@
+
+public class HyScopeInfo
+{
+	public string pesticideId
+	{
+		get;
+		set;
+	}
+
+	public string formCode
+	{
+		get;
+		set;
+	}
+
+	public string contents
+	{
+		get;
+		set;
+	}
+
+	public string cropId
+	{
+		get;
+		set;
+	}
+
+	public string cropName
+	{
+		get;
+		set;
+	}
+}
