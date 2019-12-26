b83b7e6c2618d5f0d1269555d69bfc83db874992
peter0817 <59242083+peter0817@users.noreply.github.com>
2019-12-26 03:51:45 +0000

    Create Serialize.cs

---
+++ Serialize.cs
@@ -0,0 +1,17 @@
+
+using Newtonsoft.Json;
+
+public class SerializeClass
+{
+	public string Serialize(bool ident)
+	{
+		try
+		{
+			return JsonConvert.SerializeObject(this, ident ? Formatting.Indented : Formatting.None);
+		}
+		catch
+		{
+			return string.Empty;
+		}
+	}
+}
